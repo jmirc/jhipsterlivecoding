6b499bf9fb841aa7f861a28c7785509d236f81dd
Jérôme Mirc <jerome.mirc@gmail.com>
2015-04-22 22:41:27 -0400

    Fix issue

--- content-generator-service/src/main/java/com/expedia/livecoding/mtl/service/contentgeneratorservice/ContentGeneratorServiceApplication.java
+++ content-generator-service/src/main/java/com/expedia/livecoding/mtl/service/contentgeneratorservice/ContentGeneratorServiceApplication.java
@@ -2,10 +2,8 @@ package com.expedia.livecoding.mtl.service.contentgeneratorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringCloudApplication
-@EnableHystrix
 public class ContentGeneratorServiceApplication {
 
     public static void main(String[] args) {
